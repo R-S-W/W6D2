c092f7ff4e5f6bbe4ac9f8bcba2d50aa2f4b3dd3
Ray <raysidarta@gmail.com>
2021-02-02 15:57:47 -0500

    Finish 00, work on 01

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,38 +5,82 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    if !@columns 
+
+      col_query||= DBConnection.execute2(<<-SQL)
+        SELECT * 
+        FROM #{self.table_name}
+      SQL
+      @columns = col_query.first.map{|n| n.to_sym}
+    end
+    @columns
   end
 
   def self.finalize!
+
+    columns.each do |col|
+
+       define_method(col) do 
+        attributes[col]
+      end
+
+      define_method("#{col}=") do |newval|
+        attributes[col] = newval
+      end
+    end
+
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
+    @table_name ||= self.to_s.tableize
+
     # ...
+
   end
 
   def self.all
-    # ...
+    all_query ||= DBConnection.execute(<<-SQL )
+      select #{table_name}.*
+      from #{table_name}
+    SQL
+    self.parse_all(all_query)
   end
 
   def self.parse_all(results)
-    # ...
+    class_list = []
+    results.each do |dict|
+      class_list << self.new(dict)
+    end
+    class_list
   end
 
   def self.find(id)
-    # ...
+    find_query =DBConnection.execute(<<-SQL, id)
+      select *
+      from #{table_name}
+      where ? = id
+    SQL
+    self.new(find_query.first)  unless find_query.empty?
   end
 
   def initialize(params = {})
-    # ...
+
+    params.each do |an, v|
+      if !self.class.columns.include?(an.to_sym)
+        raise "unknown attribute '#{an}'"
+      end
+      send(an.to_s+"=", v)
+        
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
@@ -44,7 +88,9 @@ class SQLObject
   end
 
   def insert
-    # ...
+    col_names = columns.map(&:to_s).join(",")
+    question_marks = ["?"]*columns.length
+    
   end
 
   def update
